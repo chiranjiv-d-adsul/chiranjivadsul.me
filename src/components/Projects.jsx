8712d14df008f2a6f3d545b9dda31903c609ff91
Chiranjiv Dhanraj Adsul🎭 <114968587+chiranjiv-d-adsul@users.noreply.github.com>
2024-04-01 03:28:08 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -139,7 +139,7 @@ const ProjectCard = ({
     <motion.div
       variants={fadeIn("right", "spring", index * 0.5, 0.75)}
       className={`relative ${
-        active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] "
+        active === id ? "lg:flex-[3.5] flex-[10]" : "lg:flex-[0.5] flex-[2]"
       } flex items-center justify-center sm:min-w-[32%] min-w-[170px]
       h-[480px] cursor-pointer card-shadow`}
       onClick={handleCardClick}
