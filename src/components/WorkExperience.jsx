b33d2371983e0d268672e50b76c93eaf2dececde
adsul chiru17 <adsulchiranjiv958@gmail.com>
2024-07-05 00:07:25 +0530

    update

--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -187,7 +187,7 @@ const WorkExperience = () => {
                   .setAttribute("src", download);
               }}
             >
-            <a href="https://drive.google.com/file/d/16XF9XMBwcKkAwZVs6LLmTRXw8T9ghnoK/view?usp=sharing">RESUME</a>
+            <a href="https://drive.google.com/file/d/1liU5AxMeGoSP-siR0-ErXW5VLVilCDoN/view?usp=sharing">RESUME</a>
               {/* <img
                 src={download}
                 alt="download"
